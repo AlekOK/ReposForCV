e1f5489ce01d034494b9e144685d2f45d6d7ce14
Antonenko Olexiy <antonenkoolexiy.com>
2020-09-01 08:16:05 +0300

    trqqussadwsdhs

---
+++ REACT_KAMA/precache-manifest.8d3166763416c4dcccf643050dc29ab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a09901918fdd2ccb1d3209b8df3cd5",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "8c895f6178ef3cd4934a",
+    "url": "/ReactApp/static/css/main.4d1f65c4.chunk.css"
+  },
+  {
+    "revision": "b834cebd6cdbf05623f3",
+    "url": "/ReactApp/static/js/2.99ec2c79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactApp/static/js/2.99ec2c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c895f6178ef3cd4934a",
+    "url": "/ReactApp/static/js/main.985ccf24.chunk.js"
+  },
+  {
+    "revision": "257cc2598db8d4d026a0",
+    "url": "/ReactApp/static/js/runtime-main.f02f0c3e.js"
+  }
+]);
